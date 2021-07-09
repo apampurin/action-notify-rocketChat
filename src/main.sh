6c836b5469fe633d352a5bba7c793a5f78c37c26
apampurin <84324370+apampurin@users.noreply.github.com>
2021-07-09 18:50:57 +0300

    Update main.sh

--- src/main.sh
+++ src/main.sh
@@ -5,14 +5,35 @@ source "$PR_SIZE_LABELER_HOME/src/rocket.sh"
 # $3 -> Group hook (opt)
 # $4,$5,$6 -> Private message params
 
+msg="$2"
+user="$4"
+pass="$5"
+to="$6"
+bot_name=${7:-CI-Bot}
+
+sendPrivateMessage(){
+  TO="@$to"
+  echo "$msg"
+  loginData="{\"user\":\"$user\",\"password\":\"$pass\"}"
+  data="{\"channel\":\"${TO}\",\"text\":\"${msg}\",\"username\":\"${BOT_NAME}\",\"avatar\":\"${BOT_AVATAR}\"}"
+  login=$(curl -s -H "Content-type:application/json" "$ROCKET_CHAT_URL$URL_LOGIN" -d "$loginData")
+  userData=$(echo "$login" | jq '.data')
+  userId=$(echo "$userData" | jq -r '.userId')
+  token=$(echo "$userData" | jq -r '.authToken')
+
+  response=$(curl -v -d "$data" -H "Content-type:application/json" -H "X-User-Id: $userId" -H "X-Auth-Token: $token" $ROCKET_CHAT_URL$SEND_MESSAGE_URL)
+  echo $response
+}
+
+
 main(){
 
 export ROCKET_CHAT_URL="$1"
-export BOT_NAME="$7"
+export BOT_NAME="$bot_name"
 export BOT_AVATAR="$8"
 
 if [[ -n "$5" ]]; then
-  rocket::sendPrivateMessage $4 $5 $6 $2
+  sendPrivateMessage
 else
   rocket::sendGroupMessage $3 $2
 fi
